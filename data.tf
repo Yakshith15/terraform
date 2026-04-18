57d5f5777bc31e4ec57b158f56afb844ae409303
Yakshith15 <daggupatiyakshithnaidu@gmail.com>
2026-04-18 22:15:59 +0530

    added custom script

--- data.tf
+++ data.tf
@@ -1,10 +1,10 @@
-data "aws_ami" "amazon_linux_2023" {
+data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["amazon"]
+  owners      = ["099720109477"]
 
   filter {
     name   = "name"
-    values = ["al2023-ami-*-x86_64"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 
   filter {
--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "ec2_instance" {
-  ami           = data.aws_ami.amazon_linux_2023.id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+
+  user_data = file("${path.module}/scripts/startup.sh")
 
   root_block_device {
     volume_size = var.root_volume_size
